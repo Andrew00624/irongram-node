fb3102c5015be358221d397085a07e456b7b26ae
Oswaldinho24k <oswalfut_96@hotmail.com>
2018-09-21 17:21:45 -0500

    almost likeable

--- models/Post.js
+++ models/Post.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const postSchema = new Schema({
+  likeado:{
+    type:Boolean,
+    default:false
+  },
   text:String,
   imageURL:String,
   user:{
--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,37 @@
 const router = require('express').Router()
 const Post = require('../models/Post')
 const uploadCloud = require('../helpers/cloudinary')
+const Comment = require('../models/Comment')
+const ObjectId = require('mongodb').ObjectID;
 
 
+//lista de posts
 router.get('/', (req, res, next)=>{
   Post.find().sort('-created_at').populate('user')
-    .then(posts=>{      
-      res.render('posts/newsfeed',{posts})
+    .then(posts=>{
+      
+      const postitos = posts.map(p=>{
+        p = Object.assign({},p)      
+        p._doc.like=false
+        const uID = ObjectId(req.user._id)
+
+        //if(p._doc.likes.includes(uID))p._doc.like=true
+        p._doc.likes.map(l=>{
+          l = ObjectId.toString(l)
+
+          console.log(l==req.user._id)
+          console.log(l)
+          console.log(req.user._id)
+          if(l==req.user._id)p._doc.like=true
+        })
+        
+        return p
+      })   
+      console.log(postitos)
+      res.render('posts/newsfeed',{postitos})
     }).catch(e=>{
       console.log(e)
-      res.redirect('/')
+      //res.redirect('/')
     })
 })
 
@@ -24,13 +46,21 @@ router.post('/', uploadCloud.single('image'),(req, res, next)=>{
     })
 })
 
+
+//detalle del post
 router.get('/detail/:id', (req, res, next)=>{
   const {id} = req.params
   Post.findById(id).populate('user')
     .then(post=>{
-      let isOwner=false
-      if(req.user._id==post.user._id)isOwner=true
-      res.render('posts/detail',{post:post, owner:isOwner})
+      Comment.find({post:post._id}).populate('user')
+        .then(comments=>{
+          let isOwner=false
+          if(req.user._id==post.user._id)isOwner=true
+          res.render('posts/detail',{post:post, owner:isOwner,comments:comments})
+        }).catch(e=>{
+          console.log(e)
+        })
+      
     }).catch('/posts')
 })
 
@@ -45,11 +75,51 @@ router.post('/detail/:id',uploadCloud.single('image'),(req, res, next)=>{
     })
 })
 
+router.post('/detail/:id/comments',(req, res, next)=>{
+  const {id} = req.params
+  req.body['post'] = id
+  req.body['user'] = req.user._id
+  Comment.create(req.body)
+    .then(comment=>{
+      res.redirect(`/posts/detail/${id}`)
+    }).catch(e=>{
+      console.log(e)
+    })
+})
+
+
+//borramos el post
 router.get('/delete/:id',(req, res, next)=>{
   const {id} = req.params
   Post.findByIdAndRemove(id)
     .then(post=>{
       res.redirect('/posts')
+    }).catch(e=>{
+      console.log(e)
+    })
+})
+
+///like-dislike post
+
+router.get('/like/:id',(req, res, next)=>{
+  const {id} = req.params
+  Post.findOne({_id:id,likes:{$in:[req.user._id]}})
+    .then(post=>{
+      if(post==null){
+        Post.findByIdAndUpdate(id,{$push:{likes:req.user._id}})
+          .then(p=>{
+            res.redirect('/posts')
+          }).catch(e=>{
+            console.log(e)
+          })
+      }else{
+        Post.findByIdAndUpdate(id,{$pull:{likes:req.user._id}})
+          .then(p=>{
+            res.redirect('/posts')
+          }).catch(e=>{
+            console.log(e)
+          })
+      }
     }).catch(e=>{
       console.log(e)
     })
